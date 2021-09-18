4d09eb07393d21c0525091c01f062317499b59a1
fsclaudio <fsclaudio@gmail.com>
2021-09-18 12:35:47 -0300

    created Repository

---
+++ backend/src/main/java/com/challenge/tinnova/repositories/VeiculosRepository.java
@@ -0,0 +1,11 @@
+package com.challenge.tinnova.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.challenge.tinnova.entities.Veiculos;
+
+@Repository
+public interface VeiculosRepository extends JpaRepository<Veiculos, Long> {
+
+}
